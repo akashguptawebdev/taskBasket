3c3171aa1cf0e948baaaaeb53494d684ee5f0e2d
akashguptawebdev <akashgupta.webdev@gmail.com>
2024-06-08 00:22:14 +0530

    link

--- server/app.js
+++ server/app.js
@@ -1,36 +1,38 @@
 import express from "express";
 import { dbConn } from "./database/db_connection.js";
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 import cors from "cors";
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import userRoutes from "./route/userRoute.js";
 import TaskRoutes from "./route/TaskRoute.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';  // Add this line to import the path module
 
-config()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+config();
 
-
-
-let app = express();
+const app = express();
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://66634d2eaa32010f5a31d4f8--candid-pika-dad7c4.netlify.app/", // Replace with your frontend origin
+    origin: "https://66634d2eaa32010f5a31d4f8--candid-pika-dad7c4.netlify.app", // Remove the trailing slash
     credentials: true // Allow sending cookies in requests
-  }))
+}));
 
-  app.use(express.static(path.join(__dirname, 'public'), {
+app.use(express.static(path.join(__dirname, 'public'), {
     setHeaders: (res, path) => {
-      if (path.endsWith('.js')) {
-        res.setHeader('Content-Type', 'application/javascript');
-      }
+        if (path.endsWith('.js')) {
+            res.setHeader('Content-Type', 'application/javascript');
+        }
     }
-  }));
+}));
 
-app.use("/api/user",userRoutes)
-app.use("/api/task",TaskRoutes)
-
+app.use("/api/user", userRoutes);
+app.use("/api/task", TaskRoutes);
 
 dbConn();
 
