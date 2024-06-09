6d428a20f7eb30524419aea53645b014950d202d
akashguptawebdev <akashgupta.webdev@gmail.com>
2024-06-09 17:47:11 +0530

    aaa

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,7 +1,5 @@
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
-);
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
 const firebaseConfig = {
     apiKey: "AIzaSyBv0dWpqyNs9l5nXQRTXBooGEP7fYV7104",
