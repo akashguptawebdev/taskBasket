6df2fb509e0ec37aeaf60525d7c13995e6c64e25
akashguptawebdev <akashgupta.webdev@gmail.com>
2024-06-09 17:52:48 +0530

    w

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,45 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    manifest: {
-      name: 'TaskBasket',
-      short_name: 'TB',
-      description: 'My Vite application as a PWA',
-      theme_color: '#ffffff',
-      icons: [
-        {
-          src: 'icons/icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'icons/icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        },
-        {
-          src: 'icons/icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable'
-        }
-      ]
-    },
-    srcDir: 'src',
-    filename: 'sw.js', // specify the custom service worker file
-    strategies: 'injectManifest' // use injectManifest strategy for custom service worker
-  })],
-  base: 'https://task-basket.vercel.app/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'TaskBasket',
+        short_name: 'TB',
+        description: 'My Vite application as a PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
+  base: '/',
   publicDir: 'public',
-  server:{
-    host:'0.0.0.0',
-  port:7543
-
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 7543
   }
-})
-
-
+});
