1e45a0c2583a474bc97d30a9f8861c9050d138b5
akashguptawebdev <akashgupta.webdev@gmail.com>
2024-06-10 10:28:20 +0530

    sws

--- client/src/pages/Landing Page/LandingPage.jsx
+++ client/src/pages/Landing Page/LandingPage.jsx
@@ -1,28 +1,40 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Landing.css";
-import {context} from "../../main"
+import { context } from "../../main";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/navBar/LandingComponent/Header/Header";
 import Features from "../../components/navBar/LandingComponent/Features/Features";
 
 const LandingPage = () => {
   const { isAuthenticated, setIsAuthenticated } = useContext(context);
-  
-  
-
-
+  const [checkIsAuthe, setCheckIsAuth] = useState(true);
   const navigateTo = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCheckIsAuth(false);
+    }, 500);
+  }, [isAuthenticated, checkIsAuthe]);
 
   if (isAuthenticated) {
     navigateTo("/mainPage");
   }
-  return (
-    <div className="main-Container px-5 md:px-20 mt-5 ">
-      <Header/>
-      <Features />
 
+  return (
+    <>
+    <div className="w-full h-screen">
+    {checkIsAuthe ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="main-Container px-5 md:px-20 mt-5 ">
+          <Header />
+          <Features />
+        </div>
+      )}
 
     </div>
+     
+    </>
   );
 };
 
